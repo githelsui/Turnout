413367d3c0c9ca4caa6897dcb71d5d4b1caa4621
Githel Lynn Suico <githelsuico@fb.com>
2020-07-13 15:43:46 -0700

    User story: Logout User with Parse

--- Turnout/ViewControllers/LiveFeedController.m
+++ Turnout/ViewControllers/LiveFeedController.m
@@ -7,6 +7,11 @@
 //
 
 #import "LiveFeedController.h"
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
+#import <FBSDKCoreKit/FBSDKProfile.h>
+#import <FBSDKLoginKit/FBSDKLoginManager.h>
+#import <Parse/Parse.h>
 
 @interface LiveFeedController ()
 
@@ -19,14 +24,22 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)logoutTapped:(id)sender {
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+    sceneDelegate.window.rootViewController = loginViewController;
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {}];
 }
-*/
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
