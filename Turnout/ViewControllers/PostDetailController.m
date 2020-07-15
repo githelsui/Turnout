ae2c2fc0deef2380cd49d58107103ca7a4557e45
Githel Lynn Suico <githelsuico@fb.com>
2020-07-15 12:10:06 -0700

    Continue Issue: Post Detail Screen's layout constraints when no image attached

--- Turnout/ViewControllers/PostDetailController.m
+++ Turnout/ViewControllers/PostDetailController.m
@@ -9,6 +9,13 @@
 #import "PostDetailController.h"
 
 @interface PostDetailController ()
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *attachedPhoto;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UIButton *likesBtn;
 
 @end
 
@@ -16,17 +23,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setUI];
+}
+
+- (void)setUI{
+    PFUser *user = self.post.author;
+    [user fetchIfNeeded];
+    self.nameLabel.text = user[@"username"];
+    self.statusLabel.text = self.post.status;
+    self.timeLabel.text = self.post.timePosted;
+    self.dateLabel.text = self.post.datePosted;
+    [self.post.image getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        if (!error) {
+            UIImage *img = [UIImage imageWithData:data];
+            self.attachedPhoto.image = img;
+        }else{
+            NSLog(@"Print error!!! %@", error.localizedDescription);
+        }
+    }];
+    [self checkImageView];
+}
+
+- (void)checkImageView{
+    if(self.post.image == nil){
+        NSLog(@"%s", "no image found");
+        CGFloat screenWidth = self.view.bounds.size.width;
+        CGFloat locationWidth = self.locationLabel.layer.frame.size.width;
+        CGSize statusSize = self.statusLabel.layer.frame.size;
+        CGPoint statusPos = self.statusLabel.layer.position;
+        [self.attachedPhoto removeFromSuperview];
+        self.attachedPhoto = nil;
+        CGPoint locPoint;
+        locPoint.x = (screenWidth + locationWidth - statusSize.width) / 2;
+        locPoint.y = statusSize.height + statusPos.y + 10;
+        CGPoint likePoint;
+        likePoint.x = screenWidth - self.likesBtn.layer.frame.size.width;
+        likePoint.y = statusSize.height + statusPos.y + 10;
+        self.locationLabel.layer.position = locPoint;
+        self.likesBtn.center = likePoint;
+    }
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
