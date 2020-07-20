dc9684a83ab0d22b1568138173a9029f17fe1d08
Githel Lynn Suico <githelsuico@fb.com>
2020-07-20 14:46:02 -0700

    Issue continued: NSArray of NSDictionaries per zipcode after reading CSV file

--- Turnout/main.m
+++ Turnout/main.m
@@ -13,6 +13,18 @@ int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
+        NSMutableArray *allData = [NSMutableArray array];
+        NSString *myPath = [[NSBundle mainBundle]pathForResource:@"USZipcodes" ofType:@"txt"];
+        NSString *myFile = [[NSString alloc]initWithContentsOfFile:myPath encoding:NSUTF8StringEncoding error:nil];
+        NSLog(@"Our file contains this: %@", myPath);
+        NSArray *rows = [myFile componentsSeparatedByString:@"\n"];
+        for(NSString *row in rows){
+            NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+            NSArray* columns = [row componentsSeparatedByString:@","];
+            dict[@"zipcode"] = columns[1];
+            dict[@"city"] = columns[2];
+            [allData addObject:dict];
+        }
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
